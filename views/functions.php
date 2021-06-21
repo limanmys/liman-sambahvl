044477c4f378e19e69b82cfc9add6c25b0f5c8b3
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2021-06-21 08:51:53 +0300

    last updates

--- views/functions.php
+++ views/functions.php
@@ -23,6 +23,8 @@ use Liman\Toolkit\Shell\Command;
         if(isFileExists($smbConfigPath) == false){
             return respond(false,200);
         }
+        
+        
     }
 
     function verifyInstallationPhp(){
@@ -165,9 +167,221 @@ use Liman\Toolkit\Shell\Command;
         return respond($output, 200);
     }
 
-    function createFile(){
-        $fileName = request('fileName');
-        runCommand('touch /home/smb/' . $fileName);
-        return respond($fileName, 200);
+    // #### FSMO-Role Management Tab ####
+    
+    function printTable(){
+        $allData = runCommand(sudo()."samba-tool fsmo show");
+        $allDataList = explode("\n",$allData);
+        $dict = [
+            "SchemaMasterRole" => "schema",
+            "InfrastructureMasterRole" => "infrastructure",
+            "RidAllocationMasterRole" => "rid",
+            "PdcEmulationMasterRole" => "pdc",
+            "DomainNamingMasterRole" => "naming",
+            "DomainDnsZonesMasterRole" => "domaindns",
+            "ForestDnsZonesMasterRole" => "forestdns",
+        ];
+        $data = [];
+        for($i=0; $i<count($allDataList); $i++){
+            $item = $allDataList[$i];
+            $itemList = explode(",",$item);
+
+            $nameItem = explode("=",$itemList[1]);
+            $nameItem = $nameItem[1];
+
+            if ($nameItem != "") {
+                $roleItem = explode(" ",$itemList[0]);
+                $roleItem = $roleItem[0];
+                $data[] = [
+                    "role" => $roleItem,
+                    "name" => $nameItem,
+                    "contraction" => $dict[$roleItem]
+                    
+                ];  
+            }
+      
+        }
+        return view('table', [
+            "value" => $data,
+            "title" => ["Rol","Sunucu","*hidden*"],
+            "display" => ["role","name","contraction:contraction"],
+            "menu" => [
+
+                "Bu rolü al" => [
+                    "target" => "takeTheRole",
+                    "icon" => "fa-file-export"
+                ],
+    
+            ],
+        ]);
+    }
+
+    function takeTheRole(){
+        $contraction = request("contraction");
+        $output=runCommand(sudo()."samba-tool fsmo transfer --role=$contraction -UAdministrator");
+        return respond($output,200);
+    }
+
+    function takeAllRoles(){
+        $output=runCommand(sudo()."samba-tool fsmo transfer --role=all -UAdministrator");
+        return respond($output,200);
+    }
+
+    function trustedServers(){
+        $allData = runCommand(sudo() . "samba-tool domain trust list 2>&1");
+        $allDataList = explode("\n", $allData);
+
+        $data=[];
+        foreach($allDataList as $item){
+            if($item){
+                $itemInfos = explode("[", $item);
+                $data[] = [
+                    "type" => substr($itemInfos[1], 0, strpos($itemInfos[1], "]")),
+                    "transitive" => substr($itemInfos[2], 0, strpos($itemInfos[2], "]")),
+                    "direction" => substr($itemInfos[3], 0, strpos($itemInfos[3], "]")),
+                    "name" => substr($itemInfos[4], 0, strpos($itemInfos[4], "]"))
+                ];
+            }
+        }                
+
+        return view('table', [
+            "value" => $data,
+            "title" => ["Names of Servers", "*hidden*", "*hidden*", "*hidden*"],
+            "display" => ["name", "type:type", "transitive:transitive", "direction:direction"],
+            "onclick" => "showTrustedServerDetailsModal",
+            "menu" => [
+
+                "Delete" => [
+                    "target" => "showDeleteTrustedServerModal",
+                    "icon" => "fas fa-trash-alt"
+                ],
+
+            ],
+        ]);
+    }
+
+    function destroyTrustRelation(){
+        $name = request("name");
+        $password = request("password");
+        $output = runCommand(sudo() . "samba-tool domain trust delete " . $name .
+                            " -U administrator@" . $name .
+                            " --password=" . $password);
+        return respond("Trust relation with " . $name . " was destroyed", 200);
+    }
+
+    function createTrustRelation(){
+        $domainName = request("newDomainName");
+        $ipAddr = request("newIpAddr");
+        $type = request("newType");
+        $direction = request("newDirection");
+        $createLocation = request("newCreateLocation");
+        $username = request("newUsername");
+        $password = request("password");
+
+        if(!($domainName && $ipAddr && $type && $direction && $createLocation && $username && $password))
+            return respond("Please fill all fields!", 202);
+
+        runCommand(sudo() . "samba-tool domain trust create " . $domainName .
+                    " --type=" . $type . " --direction=" . $direction .
+                    " --create-location=" . $createLocation . " -U " . $username .
+                    "@" . $domainName . " --password=" . $password);
+        return respond("Trust relation with " . $domainName . " has been created", 200);
+    }
+
+    function replicationOrganized(){
+        $hostNameTo = runCommand("hostname");
+
+        $allInfo = runCommand(sudo() . "samba-tool drs showrepl --json");
+        $allInfo = json_decode($allInfo,true);
+
+        $data = [];
+
+        for ($i=0; $i < count($allInfo["repsFrom"]); $i++) {
+            $pureHostName = str_replace("Default-First-Site-Name\\", "", $allInfo["repsFrom"][$i]["DSA"]);
+            $data[] = [
+                "hostNameTo" => $hostNameTo,
+                "info" => $allInfo["repsFrom"][$i]["NC dn"],
+                "hostNameFrom" => $pureHostName,
+                "lastUpdateTime" => $allInfo["repsFrom"][$i]["last success"]
+            ];
+        }
+
+        return view('table', [
+            "value" => $data,
+            "title" => ["Incoming Host Name", "Info", "Outgoing Host Name", "*hidden*"],
+            "display" => ["hostNameTo", "info", "hostNameFrom", "lastUpdateTime:lastUpdateTime"],
+            "onclick" => "test",
+            "menu" => [
+
+                "Update Replication" => [
+                    "target" => "updateReplication",
+                    "icon" => "fa-plus-circle"
+                ],
+
+                "Last Update Time" => [
+                    "target" => "showUpdateTime",
+                    "icon" => "fa-recycle"
+                ],
+            ],
+        ]);   
+    }
+
+    function createBound(){ 
+        $incomingHostName = request('inHost');
+        $outgoingHostName = request('outHost');
+        $info = request('info');
+        runCommand(sudo() . 'samba-tool drs replicate ' . $incomingHostName . $outgoingHostName . $info);
+        return respond("Islem basarili", 200);
+    }
+
+    function showUpdateTime(){
+        $lastUpdateTime = request('lastUpdateTime');
+        return respond($lastUpdateTime, 200);
+        
+    }
+
+    #### Migration Tab ####
+    
+    function migrate(){
+        $ip = request("ip");
+        $username = request("username");
+        $password = request("password");
+        runCommand(sudo()."smb-migrate-domain -s ".$ip." -a ".$username." -p ".$password,200);
+
+        if(check2() == true){
+            //migrate edilebilir yani migrate edilmemiş.
+            return respond(true,200);
+        }
+        else{
+            return respond(false,200);
+        }
+        
+
+    }
+    function check(){
+        //check => true ise migrate edilebilir.
+        $output=runCommand(sudo()."net ads info",200);
+        if($output==""){
+            $output=runCommand(sudo()."net ads info 2>&1",200);
+        }
+        if(str_contains($output, "Can't load /etc/samba/smb.conf")){
+            return respond(true,200);
+        }
+        else{
+            return respond(false,200);
+        }
+    }
+    function check2(){
+        //check => true migrate edilebilir.
+        $output=runCommand(sudo()."net ads info",200);
+        if($output==""){
+            $output=runCommand(sudo()."net ads info 2>&1",200);
+        }
+        if(str_contains($output, "Can't load /etc/samba/smb.conf")){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 ?>
--- views/index.blade.php
+++ views/index.blade.php
@@ -1,3 +1,123 @@
+@component('modal-component',[
+        "id" => "infoModal",
+        "title" => "Sonuç Bilgisi",
+        "footer" => [
+            "text" => "OK",
+            "class" => "btn-success",
+            "onclick" => "hideInfoModal()"
+        ]
+    ])
+@endcomponent
+
+@component('modal-component',[
+        "id" => "changeModal",
+        "title" => "Rol Seçimi",
+        "footer" => [
+            "text" => "AL",
+            "class" => "btn-success",
+            "onclick" => "hideChangeModal()"
+        ]
+    ])
+    @include('inputs', [
+        "inputs" => [
+            "Roller:newType" => [
+                "SchemaMasterRole" => "schema",
+                "InfrastructureMasterRole" => "infrastructure",
+                "RidAllocationMasterRole" => "rid",
+                "PdcEmulationMasterRole" => "pdc",
+                "DomainNamingMasterRole" => "naming",
+                "DomainDnsZonesMasterRole" => "domaindns",
+                "ForestDnsZonesMasterRole" => "forestdns",
+                "All" => "all"
+            ],
+        ]
+    ])
+@endcomponent
+
+@component('modal-component',[
+        "id" => "createTrustRelationModal",
+        "title" => "Create Trust Relation",
+        "footer" => [
+            "text" => "Create",
+            "class" => "btn-success",
+            "onclick" => "createTrustRelation()"
+        ]
+    ])
+    @include('inputs', [
+        "inputs" => [
+            "Domain Name" => "newDomainName:text:deneme.lab",
+            "IP Address" => "newIpAddr:text",
+            "Type:newType" => [
+                "forest" => "forest",
+                "external" => "external"
+            ],
+            "Direction:newDirection" => [
+                "incoming" => "incoming",
+                "outgoing" => "outgoing",
+                "both" => "both"
+            ],
+            "Create Location:newCreateLocation" => [
+                "local" => "local",
+                "both" => "both"
+            ],
+            "Username" => "newUsername:text",
+            "Password" => "password:password"
+        ]
+    ])
+@endcomponent
+
+@component('modal-component',[
+        "id" => "trustedServerDetailsModal",
+        "title" => "Details",
+        "footer" => [
+            "text" => "Close",
+            "class" => "btn-success",
+            "onclick" => "closeTrustedServerDetailsModal()"
+        ]
+    ])
+@endcomponent
+
+@component('modal-component',[
+        "id" => "deleteTrustedServerModal",
+        "title" => "Warning",
+        "footer" => [
+            "text" => "Cancel",
+            "class" => "btn-success",
+            "onclick" => "closeTrustedServerDetailsModal()"
+        ],
+        "footer" => [
+            "text" => "Delete",
+            "class" => "btn-danger",
+            "onclick" => "destroyTrustRelation()"
+        ]
+    ])
+        @include('inputs', [
+        "inputs" => [
+            "Password" => "password:password"
+        ]
+    ])
+@endcomponent
+
+@component('modal-component',[
+        "id" => "migrationModal",
+        "title" => "Giriş",
+        "footer" => [
+            "text" => "OK",
+            "class" => "btn-success",
+            "onclick" => "hideMigrationModal()"
+        ]
+    ])
+    @include('inputs', [
+        "inputs" => [
+            "IP Addresi" => "ipAddr:text",
+            "Kullanıcı Adı" => "username:text",
+            "Şifre" => "password:password"
+        ]
+    ])
+@endcomponent
+
+
+
 <ul class="nav nav-tabs" role="tablist" style="margin-bottom: 15px;">
     <li class="nav-item">
         <a class="nav-link active"  onclick="tab1()" href="#tab1" data-toggle="tab">Kurulum</a>
@@ -12,7 +132,18 @@
     </li>
 
     <li class="nav-item">
-        <a class="nav-link " href="#tab4" data-toggle="tab">Dosya Oluştur</a>
+        <a class="nav-link "  onclick="printTable()" href="#fsmo" data-toggle="tab">FSMO Rol Yönetimi</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link "  onclick="trustedServers()" href="#trustRelation" data-toggle="tab">Trusted Servers</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link "  onclick="replicationInfo()" href="#replication" data-toggle="tab">Replication Info</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link " onclick="migration()" href="#migration"  data-toggle="tab">Migration İşlemi</a>
     </li>
 </ul>
 
@@ -40,30 +171,39 @@
         </pre>
     </div>
 
-    <div id="tab4" class="tab-pane">
-        <button class="btn btn-success mb-2" id="asd" onclick="showFileModal()" type="button">Dosya oluştur</button>
+    <div id="fsmo" class="tab-pane">
+        <p>Tablo üzerinde sağ tuş ile bir rolü üzerinize alabilir veya bunun için butonları kullanabilirsiniz.</p>
+        <br />
+        <button class="btn btn-success mb-2" id="btn1" onclick="showInfoModal()" type="button">Tüm rolleri al</button>
+        <button class="btn btn-success mb-2" id="btn2" onclick="showChangeModal()" type="button">Belirli bir rolü al</button>
+        <div class="table-responsive" id="fsmoTable"></div>
+    </div>
+
+    <div id="trustRelation" class="tab-pane">
+        <button class="btn btn-success mb-2" id="createButton" onclick="showCreateTrustRelationModal()" type="button">Create</button>    
+        <div id="trustedServers">
+        </div>
+    </div>
+    
+    <div id="replication" class="tab-pane">
+        <div id="replicationPrintArea"></div> 
+        <div class="table-responsive replicationTable" id="replicationTable"></div> 
+    </div>
+
+    <div id="migration" class="tab-pane">
+        <h1>{{ __(' Migration İşlemi') }}</h1>
+        <br />
+        <button class="btn btn-success mb-2" id="btn3" onclick="showMigrationModal()" type="button">Migrate</button>
+        <div class="text-area" id="textarea"></div>
     </div>
 
 </div>
 
-@component('modal-component',[
-        "id" => "createFileModal",
-        "title" => "Dosya Oluştur",
-        "footer" => [
-            "text" => "Oluştur",
-            "class" => "btn-success",
-            "onclick" => "createFile()"
-        ]
-    ])
-    @include('inputs', [
-        "inputs" => [
-            "Dosya Adı" => "filename:text:Dosya adı"
-        ]
-    ])
-@endcomponent
-
 <script>
-   if(location.hash === ""){
+    var domainName = "";
+    var passwd = "";
+
+    if(location.hash === ""){
         tab1();
     }
 
@@ -190,22 +330,300 @@
         });
     }
 
-    function createFile(){
-        var form = new FormData();
-        let fileName = $('#createFileModal').find('input[name=filename]').val();
-        form.append("fileName",fileName);
+    // #### FSMO-Role Management Tab ####
 
-        request(API('createFile'), form, function(response) {
+    function printTable(){
+        showSwal('Yükleniyor...','info',2000);
+        var form = new FormData();
+        request(API('printTable'), form, function(response) {
+            $('#fsmoTable').html(response).find('table').DataTable({
+            bFilter: true,
+            "language" : {
+                url : "/turkce.json"
+            }
+            });;
+        }, function(response) {
+            let error = JSON.parse(response);
+            showSwal(error.message, 'error', 3000);
+        });
+        
+    }
+    
+    function takeTheRole(line){
+        var form = new FormData();
+        let contraction = line.querySelector("#contraction").innerHTML;
+        form.append("contraction",contraction);
+
+        request(API('takeTheRole'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            if(message == ""){
+                showSwal('Hata oluştu.', 'error', 7000);
+            }
+            else if(message.includes("successful")){
+                printTable();
+                showSwal(message,'success',7000);
+            }
+            else{
+                showSwal(message,'info',7000);
+            }
+        }, function(error) {
+            showSwal(error.message, 'error', 5000);
+
+        });
+    }
+    
+    function showInfoModal(line){
+        showSwal('Yükleniyor...','info',3500);
+        var form = new FormData();
+        request(API('takeAllRoles'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            $('#infoModal').find('.modal-body').html(
+                "<pre>"+message+"</pre>"
+            );
+            $('#infoModal').modal("show");
+        }, function(error) {
+            showSwal(error.message, 'error', 5000);
+
+        });
+    }
+
+    function hideInfoModal(line){
+        $('#infoModal').modal("hide");
+        printTable();
+    }
+
+    function showChangeModal(line){
+        showSwal('Yükleniyor...','info',2000);
+        $('#changeModal').modal("show");
+    }
+
+    function hideChangeModal(line){
+        var form = new FormData();
+        form.append("contraction", $('#changeModal').find('select[name=newType]').val());
+        request(API('takeTheRole'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            $('#changeModal').modal("hide");
+            if(message == ""){
+                showSwal('Hata oluştu.', 'error', 7000);
+            }
+            else if(message.includes("successful")){
+                printTable();
+                showSwal(message,'success',7000);
+            }
+            else{
+                showSwal(message,'info',7000);
+            }
+
+        }, function(error) {
+            $('#changeModal').modal("hide");
+            showSwal(error.message, 'error', 5000);
+        });
+    }
+
+    /**
+     * Showing the servers which have trusted relation with this server
+    */
+
+    function trustedServers(){
+        var form = new FormData();
+        request("{{API('trustedServers')}}", form, function(response) {
+            $('#trustedServers').html(response).find('table').DataTable({
+            bFilter: true,
+            "language" : {
+                url : "/turkce.json"
+            }
+          });;
+        }, function(error) {
+            $('#trustedServers').html("Hata oluştu");
+        });
+        
+    }
+
+    function showTrustedServerDetailsModal(line){
+        var name = line.querySelector("#name").innerHTML;
+        var type = line.querySelector("#type").innerHTML;
+        var transitive = line.querySelector("#transitive").innerHTML;
+        var direction = line.querySelector("#direction").innerHTML;
+        console.log(name);
+        if(name)
+            $('#trustedServerDetailsModal h4.modal-title').html("Details");
+        $('#trustedServerDetailsModal').find('.modal-body').html(
+            "Name".bold() + "</br>" + name + "</br>" + "</br>" +
+            "Type".bold() + "</br>" + type + "</br>" + "</br>" +
+            "Transitive".bold() + "</br>" + transitive + "</br>" + "</br>" +
+            "Direction".bold() + "</br>" + direction + "</br>" + "</br>"
+        );
+        $('#trustedServerDetailsModal').modal("show");
+    }
+
+    function closeTrustedServerDetailsModal(){
+        $('#trustedServerDetailsModal').modal("hide");
+    }
+
+    /**
+     * Deleting the servers which have trusted relation with this server
+    */
+
+    function closeDeleteTrustedServerModal(){
+        $('#deleteTrustedServerModal').modal("hide");
+    }
+
+    function showDeleteTrustedServerModal(line){
+        let name = line.querySelector("#name").innerHTML;
+        domainName = name;
+        $('#deleteTrustedServerModal').find('.modal-body').prepend(
+            "If you destroy trust relation with \"".bold() + name.bold() + "\", please fill the password field.".bold() + "<br><br>");
+        $('#deleteTrustedServerModal').find('.modal-footer')
+            .append('<button type="button" class="btn btn-primary" onClick="closeDeleteTrustedServerModal()">Cancel</button>');
+        $('#deleteTrustedServerModal').modal("show");
+    }
+
+    function destroyTrustRelation(){
+        var form = new FormData();
+        form.append("name", domainName);
+        passwd = $('#deleteTrustedServerModal').find('input[name=password]').val();
+        form.append("password", passwd);
+        closeDeleteTrustedServerModal();
+        trustedServers();
+        request(API('destroyTrustRelation'), form, function(response) {
             message = JSON.parse(response)["message"];
             showSwal(message, 'success', 3000);
-            $('#createFileModal').modal("hide");
         }, function(error) {
             showSwal(error.message, 'error', 3000);
         });
     }
 
-    function showFileModal(){
-        $('#createFileModal').modal("show");
+    /**
+     * Creating trust relation with a new server
+    */
+
+    function showCreateTrustRelationModal(){
+        $('#createTrustRelationModal').modal("show");
     }
-    
+
+    function createTrustRelation(){
+        var form = new FormData();
+        form.append("newDomainName", $('#createTrustRelationModal').find('input[name=newDomainName]').val());
+        form.append("newIpAddr", $('#createTrustRelationModal').find('input[name=newIpAddr]').val());
+        form.append("newType", $('#createTrustRelationModal').find('select[name=newType]').val());
+        form.append("newDirection", $('#createTrustRelationModal').find('select[name=newDirection]').val());
+        form.append("newCreateLocation", $('#createTrustRelationModal').find('select[name=newCreateLocation]').val());
+        form.append("newUsername", $('#createTrustRelationModal').find('input[name=newUsername]').val());
+        form.append("password", $('#createTrustRelationModal').find('input[name=password]').val());
+
+        $('#createTrustRelationModal').modal("hide");
+        request(API('createTrustRelation'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            showSwal(message, 'success', 10000);
+            trustedServers();
+        }, function(error) {
+            showSwal(error.message, 'error', 3000);
+        });
+    }
+
+    function replicationInfo(){
+        showSwal('{{__("Yükleniyor...")}}','info',2000);
+        var form = new FormData();
+
+        request(API('replicationOrganized'), form, function(response) {
+            $('.replicationTable').html(response).find('table').DataTable({
+            bFilter: true,
+            "language" : {
+                url : "/turkce.json"
+            }
+            });;
+        }, function(response) {
+            let error = JSON.parse(response);
+            showSwal(error.message, 'error', 3000);
+        });
+
+    }
+
+    function updateReplication(line) {
+        var form = new FormData();
+
+        let inHost = line.querySelector("#hostNameTo").innerHTML;
+        let info = line.querySelector("#info").innerHTML;
+        let outHost = line.querySelector("#hostNameFrom").innerHTML;
+
+        form.append("inHost", inHost);
+        form.append("info", info);
+        form.append("outHost", outHost);
+
+        request(API('createBound'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            showSwal(message, 'success', 3000);
+        }, function(response) {
+            let error = JSON.parse(response);
+            showSwal(error.message, 'error', 3000);
+        });
+    }
+
+    function showUpdateTime(line) {
+        var form = new FormData();
+
+        let lastUpdateTime = line.querySelector("#lastUpdateTime").innerHTML;   
+
+        form.append("lastUpdateTime", lastUpdateTime);
+
+        request(API('showUpdateTime'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            console.log(message);
+            showSwal(message, 'success', 3000);
+        }, function(response) {
+            let error = JSON.parse(response);
+            showSwal(error.message, 'error', 3000);
+        });
+    }
+
+    // #### Migration Tab ####
+
+    function migration(){
+        var form = new FormData();
+        let x = document.getElementById("btn3");
+
+        request(API('check'), form, function(response) {
+            message = JSON.parse(response)["message"];
+            if(message==false){
+                x.disabled = true;
+                $('#textarea').html("Bu sunucu migrate edilemez.");
+            }
+        }, function(error) {
+            showSwal(error.message, 'error', 5000);
+        });
+    }
+
+    function showMigrationModal(){
+        showSwal('Yükleniyor...','info',2000);
+        $('#migrationModal').modal("show");
+    }
+    function hideMigrationModal(){
+        var form = new FormData();
+        $('#migrationModal').modal("hide");
+        form.append("ip", $('#migrationModal').find('input[name=ipAddr]').val());
+        form.append("username", $('#migrationModal').find('input[name=username]').val());
+        form.append("password", $('#migrationModal').find('input[name=password]').val());
+        
+        request(API('migrate'), form, function(response) {
+            console.log(response);
+            if(response == true){
+                showSwal('Migration başarısız', 'error', 7000);
+            }
+            else if(response == false){
+                migration();
+                showSwal('Migration başarılı', 'success', 7000);
+            }
+            else if(response == ""){
+                migration();
+                showSwal('Migration başarılı', 'success', 7000);
+            }
+            else{
+                showSwal('Migration başarısız...', 'error', 7000);
+
+            }
+
+        }, function(error) {
+            showSwal(error.message, 'error', 5000);
+        });
+    }
 </script>

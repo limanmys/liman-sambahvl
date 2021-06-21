daf047a20b46dbac45fd31331caab8d9dd337148
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2021-06-21 09:33:45 +0300

    backend oop yapisina gecildi

---
+++ app/Controllers/SambaController.php
@@ -0,0 +1,330 @@
+<?php
+namespace App\Controllers;
+
+use Liman\Toolkit\Shell\Command;
+
+class SambaController{
+    function verifyInstallation(){
+        if(trim(runCommand('dpkg -s sambahvl | grep "Status" | grep -w "install" 1>/dev/null 2>/dev/null && echo "1" || echo "0"')) == "1"){
+            return respond(true,200);
+        }else{
+            return respond(false,200);
+        }
+    } 
+
+    function installSmbPackage(){   
+        $commandLine = "hostname";
+        $hostname = runCommand(sudo() . $commandLine);
+        //$newHostname =  extensionDb('machineName');
+
+        $commandLine1 = "apt install gnupg2 ca-certificates -y";
+        $commandLine2 = "echo 'deb [arch=amd64] http://depo.aciklab.org/ onyedi main' | sudo tee /etc/apt/sources.list.d/acikdepo.list";
+        $commandLine3 = "wget --no-check-certificate -qO - http://depo.aciklab.org/public.key | sudo apt-key add -";
+        $commandLine4 = "apt update";
+        $commandLine5 = "bash -c 'DEBIAN_FRONTEND=noninteractive apt install sambahvl -qqy >/tmp/smbpyLog 2>&1 & disown'";
+
+        runCommand(sudo() . $commandLine1);
+        runCommand(sudo() . $commandLine2);
+        runCommand(sudo() . $commandLine3);
+        runCommand(sudo() . $commandLine4);
+        runCommand(sudo() . $commandLine5);
+        
+        //$a = editHostsFile($hostname,$newHostname);  
+
+        return respond($a,200);
+    }
+
+    function isFileExists($filePath){
+        $existsCommand = 'test -e '. $filePath .' && echo 1 || echo 0';
+        $existsFlag = runCommand(sudo() . $existsCommand);
+
+        if($existsFlag == 1){
+            return true;
+        }
+
+        if($existsFlag == 0){
+            return false;
+        }
+
+    }
+
+    function observeInstallation(){
+        if(verifyInstallationPhp() == true){
+            $res = "smbHVL paketi zaten var !";
+            
+            return respond($res, 202);
+        }
+
+        if(verifyInstallationPhp() == false){
+            $log = runCommand(sudo() . 'cat /tmp/smbpyLog');
+            
+            return respond($log, 200);
+        }
+    }
+
+    function verifyDomain(){
+        $smbConfigPath = "/etc/samba/smb.conf";
+
+        if(isFileExists($smbConfigPath) == true){
+            return respond(true,200);
+        }
+        
+        if(isFileExists($smbConfigPath) == false){
+            return respond(false,200);
+        }
+    }
+
+    function createSambaDomain(){
+        $domainName = extensionDb('domainName');
+        $domainPassword = extensionDb('domainPassword');
+
+        $createDomainCommand = "smb-create-domain -d " . $domainName . " -p " . $domainPassword;
+        runCommand(sudo() . $createDomainCommand);
+    }
+
+    function returnDomainInformations(){
+        $domainName = extensionDb('domainName');
+        $getDomainInformationCommand = "samba-tool domain info " . $domainName;
+
+        $domainInformations = runCommand(sudo() . $getDomainInformationCommand);
+        return respond($domainInformations,200);
+    }
+
+    function returnSambaServiceStatus(){
+        $output = runCommand(sudo() . "systemctl is-active samba4.service");
+
+        if (trim($output) == "active") {
+            return respond(true,200);
+        } 
+        else {
+            return respond(false,200);
+        }
+    }
+
+    function sambaLog(){
+        $command = "systemctl status samba4.service";
+
+        $output = runCommand(sudo() . $command);
+
+        return respond($output, 200);
+    }
+
+
+    function returnRolesTable(){
+        $allData = runCommand(sudo()."samba-tool fsmo show");
+        $allDataList = explode("\n",$allData);
+        $dict = [
+            "SchemaMasterRole" => "schema",
+            "InfrastructureMasterRole" => "infrastructure",
+            "RidAllocationMasterRole" => "rid",
+            "PdcEmulationMasterRole" => "pdc",
+            "DomainNamingMasterRole" => "naming",
+            "DomainDnsZonesMasterRole" => "domaindns",
+            "ForestDnsZonesMasterRole" => "forestdns",
+        ];
+        $data = [];
+        for($i=0; $i<count($allDataList); $i++){
+            $item = $allDataList[$i];
+            $itemList = explode(",",$item);
+
+            $nameItem = explode("=",$itemList[1]);
+            $nameItem = $nameItem[1];
+
+            if ($nameItem != "") {
+                $roleItem = explode(" ",$itemList[0]);
+                $roleItem = $roleItem[0];
+                $data[] = [
+                    "role" => $roleItem,
+                    "name" => $nameItem,
+                    "contraction" => $dict[$roleItem]
+                    
+                ];  
+            }
+      
+        }
+        return view('table', [
+            "value" => $data,
+            "title" => ["Rol","Sunucu","*hidden*"],
+            "display" => ["role","name","contraction:contraction"],
+            "menu" => [
+
+                "Bu rolü al" => [
+                    "target" => "takeTheRole",
+                    "icon" => "fa-file-export"
+                ],
+    
+            ],
+        ]);
+    }
+
+    function takeTheRole(){
+        $contraction = request("contraction");
+        $output=runCommand(sudo()."samba-tool fsmo transfer --role=$contraction -UAdministrator");
+        return respond($output,200);
+    }
+
+    function takeAllRoles(){
+        $output=runCommand(sudo()."samba-tool fsmo transfer --role=all -UAdministrator");
+        return respond($output,200);
+    }
+
+    function trustedServers(){
+        $allData = runCommand(sudo() . "samba-tool domain trust list 2>&1");
+        $allDataList = explode("\n", $allData);
+
+        $data=[];
+        foreach($allDataList as $item){
+            if($item){
+                $itemInfos = explode("[", $item);
+                $data[] = [
+                    "type" => substr($itemInfos[1], 0, strpos($itemInfos[1], "]")),
+                    "transitive" => substr($itemInfos[2], 0, strpos($itemInfos[2], "]")),
+                    "direction" => substr($itemInfos[3], 0, strpos($itemInfos[3], "]")),
+                    "name" => substr($itemInfos[4], 0, strpos($itemInfos[4], "]"))
+                ];
+            }
+        }                
+
+        return view('table', [
+            "value" => $data,
+            "title" => ["Names of Servers", "*hidden*", "*hidden*", "*hidden*"],
+            "display" => ["name", "type:type", "transitive:transitive", "direction:direction"],
+            "onclick" => "showTrustedServerDetailsModal",
+            "menu" => [
+
+                "Delete" => [
+                    "target" => "showDeleteTrustedServerModal",
+                    "icon" => "fas fa-trash-alt"
+                ],
+
+            ],
+        ]);
+    }
+
+    function destroyTrustRelation(){
+        $name = request("name");
+        $password = request("password");
+        $output = runCommand(sudo() . "samba-tool domain trust delete " . $name .
+                            " -U administrator@" . $name .
+                            " --password=" . $password);
+        return respond("Trust relation with " . $name . " was destroyed", 200);
+    }
+
+    function createTrustRelation(){
+        $domainName = request("newDomainName");
+        $ipAddr = request("newIpAddr");
+        $type = request("newType");
+        $direction = request("newDirection");
+        $createLocation = request("newCreateLocation");
+        $username = request("newUsername");
+        $password = request("password");
+
+        if(!($domainName && $ipAddr && $type && $direction && $createLocation && $username && $password))
+            return respond("Please fill all fields!", 202);
+
+        runCommand(sudo() . "samba-tool domain trust create " . $domainName .
+                    " --type=" . $type . " --direction=" . $direction .
+                    " --create-location=" . $createLocation . " -U " . $username .
+                    "@" . $domainName . " --password=" . $password);
+        return respond("Trust relation with " . $domainName . " has been created", 200);
+    }
+
+    function replicationOrganized(){
+        $hostNameTo = runCommand("hostname");
+
+        $allInfo = runCommand(sudo() . "samba-tool drs showrepl --json");
+        $allInfo = json_decode($allInfo,true);
+
+        $data = [];
+
+        for ($i=0; $i < count($allInfo["repsFrom"]); $i++) {
+            $pureHostName = str_replace("Default-First-Site-Name\\", "", $allInfo["repsFrom"][$i]["DSA"]);
+            $data[] = [
+                "hostNameTo" => $hostNameTo,
+                "info" => $allInfo["repsFrom"][$i]["NC dn"],
+                "hostNameFrom" => $pureHostName,
+                "lastUpdateTime" => $allInfo["repsFrom"][$i]["last success"]
+            ];
+        }
+
+        return view('table', [
+            "value" => $data,
+            "title" => ["Incoming Host Name", "Info", "Outgoing Host Name", "*hidden*"],
+            "display" => ["hostNameTo", "info", "hostNameFrom", "lastUpdateTime:lastUpdateTime"],
+            "onclick" => "test",
+            "menu" => [
+
+                "Update Replication" => [
+                    "target" => "updateReplication",
+                    "icon" => "fa-plus-circle"
+                ],
+
+                "Last Update Time" => [
+                    "target" => "showUpdateTime",
+                    "icon" => "fa-recycle"
+                ],
+            ],
+        ]);   
+    }
+
+    function createBound(){ 
+        $incomingHostName = request('inHost');
+        $outgoingHostName = request('outHost');
+        $info = request('info');
+        runCommand(sudo() . 'samba-tool drs replicate ' . $incomingHostName . $outgoingHostName . $info);
+        return respond("Islem basarili", 200);
+    }
+
+    function showUpdateTime(){
+        $lastUpdateTime = request('lastUpdateTime');
+        return respond($lastUpdateTime, 200);
+        
+    }
+
+    #### Migration Tab ####
+    
+    function migrateDomain(){
+        $ip = request("ip");
+        $username = request("username");
+        $password = request("password");
+        runCommand(sudo()."smb-migrate-domain -s ".$ip." -a ".$username." -p ".$password,200);
+
+        if(checkMigrate2() == true){
+            //migrate edilebilir yani migrate edilmemiş.
+            return respond(true,200);
+        }
+        else{
+            return respond(false,200);
+        }
+    }
+
+    function checkMigrate(){
+        //check => true ise migrate edilebilir.
+        $output=runCommand(sudo()."net ads info",200);
+        if($output==""){
+            $output=runCommand(sudo()."net ads info 2>&1",200);
+        }
+        if(str_contains($output, "Can't load /etc/samba/smb.conf")){
+            return respond(true,200);
+        }
+        else{
+            return respond(false,200);
+        }
+    }
+    
+    function checkMigrate2(){
+        //check => true migrate edilebilir.
+        $output=runCommand(sudo()."net ads info",200);
+        if($output==""){
+            $output=runCommand(sudo()."net ads info 2>&1",200);
+        }
+        if(str_contains($output, "Can't load /etc/samba/smb.conf")){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
+?>
--- views/index.blade.php
+++ views/index.blade.php
@@ -211,7 +211,7 @@
 
     function tab1(){
         var form = new FormData();
-        request(API('verifyInstallation'), form, function(response) {
+        request(API('verify_installation'), form, function(response) {
             $('#smblast').html("");
             message = JSON.parse(response)["message"];
             let x = document.getElementById("1");
@@ -230,7 +230,7 @@
     function installSmbPackage(){
         var form = new FormData();
         $('#smbInstallStatus').html("<b>SambaHvl kuruluyor. Lütfen kayıtları takip ediniz.</b>");
-        request(API('installSmbPackage'), form, function(response) {
+        request(API('install_smb_package'), form, function(response) {
             observe();
         }, function(error) {
             $('#smbinstall').html("Hata oluştu");
@@ -239,7 +239,7 @@
     
     function observe(){
         var form = new FormData();
-        request(API('observeInstallation'), form, function(response) {
+        request(API('observe_installation'), form, function(response) {
             let json = JSON.parse(response);
             setTimeout(() => {
                 observe();
@@ -260,7 +260,7 @@
 
     function tab5(){
         var form = new FormData();
-        request(API('verifyDomain'), form, function(response) {
+        request(API('verify_domain'), form, function(response) {
             message = JSON.parse(response)["message"];
             let x = document.getElementById("createDomainButton");
             if(message == true){
@@ -278,7 +278,7 @@
     function createDomain(){
         var form = new FormData();
         $('#domainStatus').html("<b>Etki alanı oluşturuluyor. Lütfen bekleyiniz.</b>");
-        request(API('createSambaDomain'), form, function(response) {
+        request(API('create_samba_domain'), form, function(response) {
             returnDomainInformations();
         }, function(error) {
             $('#smbinstall').html("Hata oluştu");
@@ -301,7 +301,7 @@
 
     function tab2(){
         var form = new FormData();
-        request(API('tab2'), form, function(response) {
+        request(API('return_samba_service_status'), form, function(response) {
             message = JSON.parse(response)["message"];
             if(message == true){
                 isActiveButton = '<button type="button" class="btn btn-success" disabled>Samba Servisi Aktif !</button>' ;
@@ -322,7 +322,7 @@
 
     function sambaLog(){
         var form = new FormData();
-        request(API('sambaLog'), form, function(response) {
+        request(API('return_samba_service_log'), form, function(response) {
             message = JSON.parse(response)["message"];
             $('#sambaLog').html(message);
         }, function(error) {
@@ -335,7 +335,7 @@
     function printTable(){
         showSwal('Yükleniyor...','info',2000);
         var form = new FormData();
-        request(API('printTable'), form, function(response) {
+        request(API('roles_table'), form, function(response) {
             $('#fsmoTable').html(response).find('table').DataTable({
             bFilter: true,
             "language" : {
@@ -354,7 +354,7 @@
         let contraction = line.querySelector("#contraction").innerHTML;
         form.append("contraction",contraction);
 
-        request(API('takeTheRole'), form, function(response) {
+        request(API('take_the_role'), form, function(response) {
             message = JSON.parse(response)["message"];
             if(message == ""){
                 showSwal('Hata oluştu.', 'error', 7000);
@@ -375,7 +375,7 @@
     function showInfoModal(line){
         showSwal('Yükleniyor...','info',3500);
         var form = new FormData();
-        request(API('takeAllRoles'), form, function(response) {
+        request(API('take_all_roles'), form, function(response) {
             message = JSON.parse(response)["message"];
             $('#infoModal').find('.modal-body').html(
                 "<pre>"+message+"</pre>"
@@ -400,7 +400,7 @@
     function hideChangeModal(line){
         var form = new FormData();
         form.append("contraction", $('#changeModal').find('select[name=newType]').val());
-        request(API('takeTheRole'), form, function(response) {
+        request(API('take_the_role'), form, function(response) {
             message = JSON.parse(response)["message"];
             $('#changeModal').modal("hide");
             if(message == ""){
@@ -426,7 +426,7 @@
 
     function trustedServers(){
         var form = new FormData();
-        request("{{API('trustedServers')}}", form, function(response) {
+        request(API('trusted_servers'), form, function(response) {
             $('#trustedServers').html(response).find('table').DataTable({
             bFilter: true,
             "language" : {
@@ -485,7 +485,7 @@
         form.append("password", passwd);
         closeDeleteTrustedServerModal();
         trustedServers();
-        request(API('destroyTrustRelation'), form, function(response) {
+        request(API('destroy_trust_relation'), form, function(response) {
             message = JSON.parse(response)["message"];
             showSwal(message, 'success', 3000);
         }, function(error) {
@@ -512,7 +512,7 @@
         form.append("password", $('#createTrustRelationModal').find('input[name=password]').val());
 
         $('#createTrustRelationModal').modal("hide");
-        request(API('createTrustRelation'), form, function(response) {
+        request(API('create_trust_relation'), form, function(response) {
             message = JSON.parse(response)["message"];
             showSwal(message, 'success', 10000);
             trustedServers();
@@ -525,7 +525,7 @@
         showSwal('{{__("Yükleniyor...")}}','info',2000);
         var form = new FormData();
 
-        request(API('replicationOrganized'), form, function(response) {
+        request(API('replication_organized'), form, function(response) {
             $('.replicationTable').html(response).find('table').DataTable({
             bFilter: true,
             "language" : {
@@ -550,7 +550,7 @@
         form.append("info", info);
         form.append("outHost", outHost);
 
-        request(API('createBound'), form, function(response) {
+        request(API('create_bound'), form, function(response) {
             message = JSON.parse(response)["message"];
             showSwal(message, 'success', 3000);
         }, function(response) {
@@ -566,7 +566,7 @@
 
         form.append("lastUpdateTime", lastUpdateTime);
 
-        request(API('showUpdateTime'), form, function(response) {
+        request(API('show_update_time'), form, function(response) {
             message = JSON.parse(response)["message"];
             console.log(message);
             showSwal(message, 'success', 3000);
@@ -582,7 +582,7 @@
         var form = new FormData();
         let x = document.getElementById("btn3");
 
-        request(API('check'), form, function(response) {
+        request(API('check_migrate'), form, function(response) {
             message = JSON.parse(response)["message"];
             if(message==false){
                 x.disabled = true;
@@ -597,6 +597,7 @@
         showSwal('Yükleniyor...','info',2000);
         $('#migrationModal').modal("show");
     }
+
     function hideMigrationModal(){
         var form = new FormData();
         $('#migrationModal').modal("hide");
@@ -604,7 +605,7 @@
         form.append("username", $('#migrationModal').find('input[name=username]').val());
         form.append("password", $('#migrationModal').find('input[name=password]').val());
         
-        request(API('migrate'), form, function(response) {
+        request(API('migrate_domain'), form, function(response) {
             console.log(response);
             if(response == true){
                 showSwal('Migration başarısız', 'error', 7000);

d440b3e84acae1828147fd4a05ba5691b7fa9aa7
abugraokkali <abugraokkali@outlook.com>
2021-06-23 15:19:57 +0300

    new tabs have been added.

--- views/index.blade.php
+++ views/index.blade.php
@@ -41,6 +41,29 @@
         Replication Info</a>
     </li>
 
+    <li class="nav-item">
+        <a class="nav-link "  onclick="listUsers()" href="#users" data-toggle="tab">
+        <i class="fas fa-user mr-2"></i>
+        Kullanıcılar</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link " href="#groups" data-toggle="tab">
+        <i class="fas fa-users mr-2"></i>
+        Gruplar</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link "onclick="listComputers()" href="#computers"  data-toggle="tab">
+        <i class="fas fa-desktop mr-2"></i>
+        Bilgisayarlar</a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" onclick="listSites()" href="#sites"  data-toggle="tab">
+        <i class="fas fa-network-wired mr-2"></i>
+        Site Listesi</a>
+    </li>
 </ul>
 
 <div class="tab-content">
@@ -70,6 +93,22 @@
     
     <div id="replication" class="tab-pane">
         @include('pages.replication')
+    </div>
+
+    <div id="users" class="tab-pane">
+        @include('pages.users')
+    </div>
+
+    <div id="groups" class="tab-pane">
+        @include('pages.groups')
+    </div>
+
+    <div id="computers" class="tab-pane">
+        @include('pages.computers')
+    </div>
+
+    <div id="sites" class="tab-pane">
+        @include('pages.sites')
     </div>
 
 </div>

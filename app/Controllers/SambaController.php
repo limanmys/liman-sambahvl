f133f3b4ac5fa52395be2ea71b137952da6d9b13
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2021-06-24 17:10:27 +0300

    ss

--- app/Controllers/SambaController.php
+++ app/Controllers/SambaController.php
@@ -36,7 +36,7 @@ class SambaController{
         }
     }
 
-    function installSmbPackage(){   
+    /*function installSmbPackage(){   
         $commandLine1 = "apt install gnupg2 ca-certificates -y";
         $commandLine2 = "echo 'deb [arch=amd64] http://depo.aciklab.org/ onyedi main' | sudo tee /etc/apt/sources.list.d/acikdepo.list";
         $commandLine3 = "wget --no-check-certificate -qO - http://depo.aciklab.org/public.key | sudo apt-key add -";
@@ -50,7 +50,26 @@ class SambaController{
         runCommand(sudo() . $commandLine5);
 
         return respond($a,200);
-    }
+    }*/
+
+    function installSmbPackage()
+	{
+		return respond(
+			view('components.task', [
+				'onFail' => 'onTaskFail',
+				'onSuccess' => 'onTaskSuccess',
+				'tasks' => [
+					0 => [
+						'name' => 'InstallPackage',
+						'attributes' => []
+					]
+				]
+			]),
+			200
+		);
+	}
+
+
 
 
     function observeInstallation(){
---
+++ app/Tasks/InstallPackage.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tasks;
+
+use Liman\Toolkit\Formatter;
+use Liman\Toolkit\OS\Distro;
+use Liman\Toolkit\RemoteTask\Task;
+use Liman\Toolkit\Shell\Command;
+
+class InstallPackage extends Task
+{
+	protected $description = 'Installing package...';
+	protected $sudoRequired = true;
+
+	public function __construct(array $attrbs=[])
+	{
+
+		$this->control = Distro::debian('apt|dpkg')
+			->get();
+
+		$this->command = Distro::debian(
+			"echo 'deb [arch=amd64] http://depo.aciklab.org/ onyedi main' | sudo tee /etc/apt/sources.list.d/acikdepo.list"
+		)
+
+			->get();
+		
+		$this->command = Distro::debian(
+			"wget --no-check-certificate -qO - http://depo.aciklab.org/public.key | sudo apt-key add -"
+		)
+			->get();
+
+		$this->command = Distro::debian(
+			"apt update"
+			)
+				
+			->get();
+		$this->command = Distro::debian(
+			"bash -c 'DEBIAN_FRONTEND=noninteractive apt install sambahvl -qqy >/tmp/smbHvlLog.txt 2>&1 & disown'"
+		)
+				
+			->get();
+
+		$this->attributes = $attrbs;
+		$this->logFile = Formatter::run('/tmp/smbHvlLog.txt');
+	}
+
+}
+
+		
+	
+	
+		
+		

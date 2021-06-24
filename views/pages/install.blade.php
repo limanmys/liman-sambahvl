5ddc8a3d39f444a348a156ba056f95457ba9e8cc
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2021-06-24 14:24:54 +0300

    installation info eklendi

--- views/pages/install.blade.php
+++ views/pages/install.blade.php
@@ -1,15 +1,11 @@
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-  <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </symbol>
-</svg>
-
-
-<div id="errorDiv" style="visibility:none;">
-    
+<div id="errorDiv" style="visibility:none;"></div>
+<div class="alert alert-primary d-flex align-items-center" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:" style="margin-right:1%;"><use xlink:href="#info-fill"/></svg>
+  <div>
+  <p>SambaHVL paketini kurmak için lütfen aşağıdaki butonu kullanın.</p>
+  </div>
 </div>
 
-<p>SambaHVL paketini kurmak için lütfen aşağıdaki butonu kullanın.</p>
 <button class="btn btn-success mb-2" id="install" onclick="installSmbPackage()" style="float:left;">SambaHVL Paketini Kur</button>
 <button class="btn btn-danger mb-2" id="delete" style="float:left;margin-left:10px;visibility:hidden;"></button>
 <div id="smbInstallStatus"> </div>

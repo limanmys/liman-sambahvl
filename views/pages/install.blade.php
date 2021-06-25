d44a9e3ffec8693a04079e41a64c13d9dbee4151
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2021-06-25 11:31:26 +0300

    remove steps added

--- views/pages/install.blade.php
+++ views/pages/install.blade.php
@@ -5,7 +5,7 @@
 @endcomponent
 
 <div id="errorDiv" style="visibility:none;"></div>
-<div class="alert alert-primary d-flex align-items-center " role="alert">
+<div class="alert alert-primary d-flex align-items-center " role="alert" id="infoAlert">
     <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
     <i class="fas fa-icon mr-2"></i>
     <div>
@@ -15,16 +15,12 @@
 
 <button class="btn btn-success mb-2" id="install" onclick="installSmbPackage()" style="float:left;">SambaHVL Paketini Kur</button>
 <button class="btn btn-danger mb-2" id="delete" style="float:left;margin-left:10px;visibility:hidden;"></button>
-<div id="smbInstallStatus"> </div>
-<pre id="smbinstall" style="margin-top:5%;"></pre>
-<div id="smblast">  </div>
 
 <script>
 // Install SambaHvl Package == Tab 1 == 
     function tab1(){
         var form = new FormData();
-        request(API('verify_installation'), form, function(response) {
-            $('#smblast').html("");
+        request(API('verify_installation'), form, function(response) { 
             message = JSON.parse(response)["message"];
             let x = document.getElementById("install");
             if(message == true){
@@ -34,23 +30,11 @@
                 x.disabled = false;
             }
         }, function(error) {
-            let x = document.getElementById("install");
-            x.disabled = true;
-            $('#errorDiv').html(
-                '<div class="alert alert-danger d-flex align-items-center"  role="alert">' +
-                    '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg>' +
-                    '<div>'+
-                        'Hata : Sunucuda kurulu Samba Paketi tespit edildi !'+
-                    '</div>'+
-                '</div>');
-            
-                let deleteButton = document.getElementById("delete");
-                deleteButton.onclick = function() {deleteSambaPackage()};
-                deleteButton.innerText = "Samba Paketini Kaldır";
-                deleteButton.style.visibility = "visible";
+            removeSambaPackageSteps();
         });
     }
-    function deleteSambaPackage(){
+
+    function removeSambaPackage(){
         var form = new FormData();
         showSwal("Samba paketi kaldırılıyor.", 'info', 3000);
         request(API('delete_smb_package'), form, function(response) {
@@ -61,6 +45,29 @@
             console.log(error);
         });
     }
+
+    function removeSambaPackageSteps(){
+        let installButton = document.getElementById("install");
+        installButton.disabled = true;
+
+        let infoAlert = document.getElementById("infoAlert");
+        infoAlert.remove();
+
+        $('#errorDiv').html(
+            '<div class="alert alert-danger d-flex align-items-center"  role="alert">' +
+                '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg>' +
+                '<div>'+
+                    'Hata : Sunucuda kurulu Samba Paketi tespit edildi !'+
+                '</div>'+
+            '</div>');
+            
+        let deleteButton = document.getElementById("delete");
+        deleteButton.onclick = function() {removeSambaPackage()};
+        deleteButton.innerText = "Samba Paketini Kaldır";
+        deleteButton.style.visibility = "visible";
+    }
+
+
     function installSmbPackage(){
         var form = new FormData();
         showSwal('{{__("Loading")}}...','info',2000);
@@ -75,31 +82,15 @@
             showSwal(error,'error',2000);
       })
     }
+
     function onTaskSuccess(){
         showSwal('{{__("Your request has been successfully completed")}}', 'success', 2000);
         setTimeout(function(){
           $('#packageInstallerModal').modal("hide"); 
         }, 2000);
     }
+
     function onTaskFail(){
         showSwal('{{__("An error occurred while processing your request")}}!', 'error', 2000);
-    }
-    
-    function observe(){
-        var form = new FormData();
-        request(API('observe_installation'), form, function(response) {
-            let json = JSON.parse(response);
-            setTimeout(() => {
-                observe();
-            }, 1000);
-          $("#smbinstall").text(json["message"]);
-        }, function(response) {
-            let error = JSON.parse(response);
-            if(error["status"] == 202){
-            $('#smblast').html(error);
-           } else{
-            showSwal(error, 'error', 3000);
-           }
-        });
     }
 </script>

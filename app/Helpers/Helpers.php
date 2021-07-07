64bdb4ec7a71113d85cd16f49649a2c3106a7ecd
abugraokkali <abugraokkali@outlook.com>
2021-07-07 09:44:38 +0300

    helpers added

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -17,3 +17,37 @@ if (!function_exists('checkPort')) {
 		}
 	}
 }
+
+if (!function_exists('certificateExists')) {
+    function certificateExists($ip, $port)
+    {
+        $filename = "liman-" . strtolower($ip) . "_" . $port . ".crt";
+        $path = "/usr/local/share/ca-certificates/";
+        $flag = current(preg_grep("/".preg_quote($filename)."/i", glob("$path/*")));
+        if(!$flag){
+                $filename = "liman-" . gethostbyname($ip) . "_" . $port . ".crt";
+                return current(preg_grep("/".preg_quote($filename)."/i", glob("$path/*")));
+        }
+        return $flag;
+    }
+}
+
+if (!function_exists('ldapCheck')) {
+    function ldapCheck($domainname, $user, $pw, $ip, $port) 
+    {
+        restoreHandler();
+        $username = $user."@".$domainname;
+        $pass = $pw;
+        $server = 'ldaps://'.$ip . ':' . $port;
+        
+        $ldap = ldap_connect($server);
+        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldap, LDAP_OPT_X_TLS_REQUIRE_CERT, LDAP_OPT_X_TLS_NEVER);
+        ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+        
+        $bind = @ldap_bind($ldap, $username, $pass);
+
+        setHandler();
+        return $bind;
+    }
+}

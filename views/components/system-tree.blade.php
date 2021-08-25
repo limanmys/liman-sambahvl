8f84ae4b8d4c351e0871a88ee363e1487b887c6d
abugraokkali <abugraokkali@outlook.com>
2021-08-25 12:32:43 +0300

    tree color feature

--- views/components/system-tree.blade.php
+++ views/components/system-tree.blade.php
@@ -1,15 +1,50 @@
 <script>
 
+let myTree = {
+    "MainServer": {
+        "type": "Domain",
+        "SubServer1": {
+            "type": "Site",
+            "AltServer1": {},
+            "AltServer2": {}
+        },
+        "SubServer2": {
+            "type": "Site",
+            "AltServer5": {},
+            "AltServer4": {},
+            "AltServer3": {}
+        },
+        "SubServer3": {
+            "AltServer6": {}
+        },
+        "SubServer4": {
+            "type": "DC"
+        }
+    }
+  }
+
   function getTreeJSON(){  
 
     var form = new FormData();
     request(API('get_tree_json'), form, function(response) {
         response = JSON.parse(response);
         message = JSON.parse(response.message);
-        
+        console.log(message);
         newTraverse(message, $(".tree"));
 
         $(".tree").find("ul").each(function (idx, elem) {
+          if ($(elem).attr("data-type")) {
+              if ($(elem).attr("data-type") == "Domain") {
+                $(elem).parent().find("span").first().css("border", "2px #287ed6 solid");
+              }
+              if ($(elem).attr("data-type") == "Site") {
+                $(elem).parent().find("span").first().css("border", "2px #98ff98 solid");
+              }
+              
+              if ($(elem).attr("data-type") == "DC") {
+                $(elem).parent().find("span").first().css("border", "2px #663399 solid");
+              }
+          }
           if(elem.innerHTML.trim() == "") {
             elem.remove();
           }
@@ -23,15 +58,22 @@
 
   function newTraverse (obj, parent) {
     for (let item in obj) {
-      addToTree(item, parent)
+      addToTree(item, parent, obj[item].type)
       if (obj[item] !== null && typeof(obj[item]) == "object") {
         newTraverse(obj[item], $(`ul[data-id="${item}"]`));
       }
     }
   }
 
-  function addToTree(key, parent) {
-    parent.append(`<li><span>${key}</span><ul data-id="${key}"></ul></li>`);
+  function addToTree(key, parent, type) {
+    if (key == "type") {
+      return;
+    }
+    if (type) {
+      parent.append(`<li><span>${key}</span><ul data-id="${key}" data-type="${type}"></ul></li>`);
+    } else {
+      parent.append(`<li><span>${key}</span><ul data-id="${key}"></ul></li>`);
+    }
   }
 
   setTimeout(() => {
@@ -91,7 +133,7 @@
 
 .tree code,
 .tree span {
-    border: solid 1px #1a202c;
+    border: solid 2px #1a202c;
     border-radius: .2em;
     display: inline-block;
     margin: 0 .2em .5em;

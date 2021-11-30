4c9a29d00d0de1a8c2f3971ece40747a71ab6a1c
abugraokkali <abugraokkali@outlook.com>
2021-11-30 16:25:55 +0300

    unnecessary checks are deleted

--- views/pages/groups.blade.php
+++ views/pages/groups.blade.php
@@ -1,8 +1,3 @@
-@if (certificateExists(server()->ip_address, 636))
-    @if (isCertificateValid(server()->ip_address, 636))
-        
-        @if (ldapCheck(strtolower(extensionDb('domainName')), "administrator", extensionDb('domainPassword'), server()->ip_address, 636))
-
             <div class="alert alert-primary d-flex align-items-center " id="infoDivGroups" role="alert">
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg>
             <i class="fas fa-icon mr-2"></i>
@@ -201,49 +196,3 @@
            
            
            </script>
-        @else
-            <div id="noLDAPDiv2" style="visibility:none;">
-            <div class="alert alert-danger d-flex align-items-center"  role="alert">
-                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg>
-                    <i class="fas fa-icon mr-2"></i>
-                    <div>
-                        {{__("Hata : LDAP\'a Bağlanılamadı !")}}
-                    </div>
-                    </div>
-            </div>
-                
-        @endif
-    @else
-            <div id="invalidCertificate2" style="visibility:none;">
-                <div class="alert alert-danger d-flex align-items-center"  role="alert">
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg>
-                    <i class="fas fa-icon mr-2"></i>
-                    <div>
-                        {{__("Hata : Sertifikanız hatalı veya güncel değil !")}}
-                    </div>
-                    </div>
-            </div>
-    @endif
-
-@else
-    <div id="noCertificateDiv2" style="visibility:none;"></div>
-    <script>
-    
-        $('#noCertificateDiv2').html(
-            '<div class="alert alert-danger" role="alert">' +
-            '<h4 class="alert-heading">Hata !</h4>' +
-            '<p>Sunucuda bağlantı sertifikası bulunamadı !</p>' +
-            '<hr>' +
-            
-            '<p class="mb-0">'+
-                '<a href="/ayarlar/sertifika?hostname={{server()->ip_address}}&port=636"> ' +
-                '{{__("Buraya tıklayarak sunucunuza sertifika ekleyebilirsiniz.")}}'+
-                '</a>' +
-            '</p>' +
-            '</div>'
-            );
-        
-    
-    </script>
-
-@endif
--- views/pages/organizations.blade.php
+++ views/pages/organizations.blade.php
@@ -1,9 +1,3 @@
-@if (certificateExists(server()->ip_address, 636))
-    @if (isCertificateValid(server()->ip_address, 636))
-        @if (ldapCheck(strtolower(extensionDb('domainName')), "administrator", extensionDb('domainPassword'), server()->ip_address, 636))
-
-
-
 <div class="row">
     <div class="col-sm-6">
         <div class="card">
@@ -144,65 +138,4 @@ function listObjects(path = null){
     }
 }
 
-
 </script>
-
-@else
-            <div id="noLDAPDiv" style="visibility:none;"></div>
-                <script>
-                    function listUsers(){
-                        $('#noLDAPDiv').html(
-                            '<div class="alert alert-danger d-flex align-items-center"  role="alert">' +
-                            '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg>' +
-                            '<i class="fas fa-icon mr-2"></i>' +
-                            '<div>'+
-                                '{{__("Hata : LDAP\'a Bağlanılamadı !")}}'+
-                            '</div>'+
-                            '</div>'
-                            );
-                    }
-                    
-                </script>
-        @endif
-    @else
-        <div id="invalidCertificate" style="visibility:none;"></div>
-        <script>
-            function listUsers(){
-                $('#invalidCertificate').html(
-                    '<div class="alert alert-danger d-flex align-items-center"  role="alert">' +
-                    '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill" /></svg>' +
-                    '<i class="fas fa-icon mr-2"></i>' +
-                    '<div>'+
-                        '{{__("Hata : Sertifikanız hatalı veya güncel değil !")}}'+
-                    '</div>'+
-                    '</div>'
-                    );
-            }
-            
-            
-        </script>
-    @endif
-@else
-    <div id="noCertificateDiv" style="visibility:none;"></div>
-    <script>
-    function listUsers(){
-        $('#noCertificateDiv').html(
-            '<div class="alert alert-danger" role="alert">' +
-            '<h4 class="alert-heading">Hata !</h4>' +
-            '<p>Sunucuda bağlantı sertifikası bulunamadı !</p>' +
-            '<hr>' +
-            
-            '<p class="mb-0">'+
-                '<a href="/ayarlar/sertifika?hostname={{server()->ip_address}}&port=636"> ' +
-                '{{__("Buraya tıklayarak sunucunuza sertifika ekleyebilirsiniz.")}}'+
-                '</a>' +
-            '</p>' +
-            '</div>'
-            
-            );
-        }
-
-    
-    </script>
-
-@endif
